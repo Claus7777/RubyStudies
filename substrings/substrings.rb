9f28cf45e91df6e7a6d1ef8eb864483be1fed623
claus7777 <lucasmartinsoliveira95@gmail.com>
2025-04-30 14:31:00 -0300

    Novos exercícios concluídos - Escolhedor de dias de vendas e compras de ações e analizador de substring

---
+++ substrings/substrings.rb
@@ -0,0 +1,19 @@
+def substrings(string, dictionary)
+    result_hash = {}
+    string_array = string.downcase.split(/ /)
+    dictionary.each do |word|
+        string_array.each do |string_part|
+            if string_part[word]
+                if result_hash[word] == nil
+                    result_hash[word] = 1
+                else
+                    result_hash[word] += 1
+                end
+            end
+        end
+    end
+    return result_hash
+end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+puts(substrings("Howdy partner, sit down! How's it going?", dictionary))
