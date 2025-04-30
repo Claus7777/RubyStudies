cbe877ea2b3e7aa8d1776c4d0ac45df56c459beb
claus7777 <lucasmartinsoliveira95@gmail.com>
2025-04-30 17:02:39 -0300

    Rubocop passou o pente fino

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,29 +1,27 @@
 def caesar_cipher(string, shift)
-    result = []
-    string.each_char do |char|
-        if char.match(/[[:alpha:]]/)
-            if ((char.ord <= 90 ) && (char.ord + shift > 90)) || ((char.ord <= 122) && (char.ord + shift > 122))
-                result.push((char.ord - (26 - shift)).chr)
-            else 
-                result.push((char.ord + shift).chr)
-            end
-        else
-            result.push(char)
-        end
+  result = []
+  string.each_char do |char|
+    if char.match(/[[:alpha:]]/)
+      if ((char.ord <= 90) && (char.ord + shift > 90)) || ((char.ord <= 122) && (char.ord + shift > 122))
+        result.push((char.ord - (26 - shift)).chr)
+      else
+        result.push((char.ord + shift).chr)
+      end
+    else
+      result.push(char)
     end
-    return result.join()
+  end
+  result.join
 end
 
-puts("Digite a palavra ou frase para ser encriptada:")
-variavel = gets.chomp()
-puts("Digite por quantas letras você quer que o shift seja:")
-shift = ""
+puts('Digite a palavra ou frase para ser encriptada:')
+variavel = gets.chomp
+puts('Digite por quantas letras você quer que o shift seja:')
+shift = ''
 
 until shift.match(/[[:digit:]]/)
-    shift = gets.chomp()
-    unless shift.match(/[[:digit:]]/)
-        puts("Isso não é um numero. Tente de novo")
-    end
+  shift = gets.chomp
+  puts('Isso não é um numero. Tente de novo') unless shift.match(/[[:digit:]]/)
 end
 
 puts(caesar_cipher(variavel, shift.to_i))
---
+++ colorize/main.rb
@@ -0,0 +1,5 @@
+require 'colorize'
+
+puts 'Red is 3x as fast as a normal string'.colorize(:red)
+puts 'It ain\'t easy being green...'.colorize(:green)
+puts 'I\'m blue da ba dee da boo da'.colorize(:blue)
