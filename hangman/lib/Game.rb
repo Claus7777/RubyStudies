979be5271eb2edbba1061bb8b47e92f19d115ee9
claus7777 <lucasmartinsoliveira95@gmail.com>
2025-05-16 15:40:34 -0300

    More tests implemented + load game rewritten to catch more exceptions.

--- hangman/lib/Game.rb
+++ hangman/lib/Game.rb
@@ -24,8 +24,17 @@ class Game
     end
 
     def load_game(file)
-        data = JSON.load file
-        Game.new(data['chosen_word'], data['total_lives'], data['right_letters'], data['wrong_letters'], data['current_lives'], data['turn_counter'])
+        ##TODO: Rescue loop caso o arquivo não seja JSON, não seja válido, ou não seja um JSON de arquivo de jogo
+        begin
+            data = JSON.parse(file)     
+            unless data.has_key?('chosen_word') && data.has_key?('total_lives') && data.has_key?('right_letters') && data.has_key?('wrong_letters') && data.has_key?('current_lives') && data.has_key?('turn_counter')
+                raise StandardError.new "Not a valid saved game."
+            end
+        rescue JSON::ParserError, TypeError => e
+            return e.to_s
+        else 
+            Game.new(data['chosen_word'], data['total_lives'], data['right_letters'], data['wrong_letters'], data['current_lives'], data['turn_counter'])
+        end
     end
    
 end
--- hangman/tests.rb
+++ hangman/tests.rb
@@ -13,9 +13,27 @@ describe Game do
         expect(game.save_game).to eq({"chosen_word"=>"test", "total_lives"=>7, "right_letters"=>[""], "wrong_letters"=>[""], "current_lives"=>7, "turn_counter"=>0}.to_json)
     end
 
-    it "load game" do
+    it "load game and save it again" do
         game = Game.new("test", 7, [""], [""], 7, 0)
         saved_game = game.save_game
         expect(game.load_game(saved_game).save_game).to eq({"chosen_word"=>"test", "total_lives"=>7, "right_letters"=>[""], "wrong_letters"=>[""], "current_lives"=>7, "turn_counter"=>0}.to_json)
+    end
+
+    it "load game error" do 
+        game = Game.new("test", 7, [""], [""], 7, 0)
+        saved_game = nil
+        expect(game.load_game(saved_game)).to eq("no implicit conversion of nil into String")
+    end
+
+    it 'load game invalid format' do
+        game = Game.new("test", 7, [""], [""], 7, 0)
+        saved_game = {"hello" => 3, "is there anybody in there" => 2}.to_json
+        expect{game.load_game(saved_game)}.to raise_error( StandardError, "Not a valid saved game." )
+    end
+
+    it 'load game with missing keys' do
+        game = Game.new("test", 7, [""], [""], 7, 0)
+        saved_game = {"total_lives"=>7, "right_letters"=>[""], "wrong_letters"=>[""], "current_lives"=>7}.to_json
+        expect{game.load_game(saved_game)}.to raise_error( StandardError, "Not a valid saved game." )
     end
 end
