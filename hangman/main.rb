7bc6c1b5e4cfc226714cacd3b7407f84f1897b21
claus7777 <lucasmartinsoliveira95@gmail.com>
2025-05-16 18:09:49 -0300

    Placeholder game logic in place

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,17 +1,38 @@
 # frozen_string_literal: true
 
+require_relative 'lib/Game'
+require_relative 'lib/SaveUtility'
+include SaveUtility
+
 # #Requerimentos:
-## Carregar o dicionário, escolher uma palavra aleatória entre 5 e 12 caracteres de tamanho
-## Quando o programa carrega pela primeira vez, perguntar se o Jogador quer carregar algum de seus jogos salvos
 ## Todo turno, o jogador pode escolher entre tentar adivinhar uma letra ou salvar o jogo
 ## O jogo acaba quando acabarem as vidas do jogador (7?) ou quando a palavra for totalmente acertada
 
 # #Funcoes:
-## Carregar o dicionario
-## Ir para um numero aleatorio entre 1 e 10000. Ver se essa palavra é maior que 5 e menor que 12. Se sim, ir pro próximo passo. Se não, tentar de novo
-## Pegar o estado atual do jogo: Palavra escolhida, letras acertadas, vidas totais, vidas usadas, numero do turno, quais letras já foram arriscadas e não estão nas palavras
-## Carregar um JSON com os fields: Palavra escolhida, letras acertadas, vidas totais, vidas usadas, numero do turno, letras apostadas em um game state
-## Ler uma letra do usuário do console, verificar se é uma letra mesmo. Se não for, tentar de novo. Se for, limpar input para ser minúsculo.
+## Ler uma letra do usuário do console, verificar se é uma letra mesmo. Se não for, tentar de novo. Se for, formatar input para ser minúsculo.
 ## Pegar essa letra, verificar se ela está na palavra. Se sim, mostrar a letra na palavra. Se não, botar no array de letras apostadas e tirar uma vida.
 
-def game_master; end
+def game_master
+    begin
+    puts "Welcome to Hangman! Would you like start a new game or load a saved game?"
+    puts "1 - NEW GAME"
+    puts "2 - LOAD GAME"
+
+    selector = Kernel.gets.match(/\d+/)[0]
+    selector = selector.to_i
+    raise "Invalid choice. Try again." if selector != 1 || selector != 2
+    rescue StandardError => e
+        puts e
+        retry
+    else
+        if selector == 1
+            game = Game.new(7, [''], [''], 7, 0)
+            ##TODO: the rest of the game logic
+        else
+            #TODO: logic to select a file
+            game = SaveUtility::load_game('placeholder')
+        end
+    end
+
+
+end
